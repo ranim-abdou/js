e21675e77d97c68ce3a24037b07cb80503d2bf0a
ranim-abdou <rany99s5@gmail.com>
2022-05-09 18:09:11 +0200

    n

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
-var myNome = 'Ranim';
-var surname = 'Abdou';
-var age = 22;
-var city = 'Milano';
-var email = 'rany99s5gmail.com';
+var myNome= prompt('nome');
+var surname =prompt('cognome');
+var age =prompt (eta);
+var city =prompt ('citta');
+var email =prompt('email');
 
 
 
 
-
-window.prompt('Nome');
+/*window.prompt('Nome');
 window.prompt('Cognome');
 window.prompt('Età');
 window.prompt('Città');
-window.prompt('Email');
+window.prompt('Email'); */
 
-console.log(myNome +' '+ surname + ' '+ age+' ' +city+' '+(email));
+/*console.log(myNome +' '+ surname + ' '+ age+' ' +city+' '+(email));
 
-document.write(myNome +' '+ surname + ' '+ age+' ' +city+' '+(email));
+document.write(myNome +' '+ surname + ' '+ age+' ' +city+' '+(email));*/
 
-window.alert(myNome +' '+ surname + ' '+ age+' ' +city+' '+(email));
+var fullName=myNome +' '+ surname + ' '+ age+' ' +city+' ' +('+ email +');
+window.alert(fullName);
+document.write(fullName);
+console.log(fullName);
+document.getElementById('nome').innerHTML = myName;
+document.getElementById('cognome').innerHTML = surname;
+document.getElementById('eta').innerHTML = age;
+document.getElementById('citta').innerHTML = city;
+document.getElementById('email').innerHTML = email;
