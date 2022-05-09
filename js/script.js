70be3e642d30f6b3c57a5a94c2f7b3a4896699a0
ranim-abdou <rany99s5@gmail.com>
2022-05-09 17:09:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var myNome = 'Ranim';
+var surname = 'Abdou';
+var age = 22;
+var city = 'Milano';
+var email = 'rany99s5gmail.com';
+
+
+
+
+
+window.prompt('Nome', myNome);
+window.prompt('Cognome', surname);
+window.prompt('Età', age);
+window.prompt('Città', city);
+window.prompt('Email', email);
+
+console.log(myNome +' '+ surname + ' '+ age+' ' +city+' '+(email));
+
+document.write(myNome +' '+ surname + ' '+ age+' ' +city+' '+(email));
+
+window.alert(myNome +' '+ surname + ' '+ age+' ' +city+' '+(email));
